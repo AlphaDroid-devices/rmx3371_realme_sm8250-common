aospa-op8/android_device_oneplus_sm8250-common

sm8250-common: Import sensors/2.0/multihal
Change-Id: I2615438912974f2f0f57fd96b930d4bd11d0de5b
Signed-off-by: LibXZR <i@xzr.moe>

common.mk
@@ -447,7 +447,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@4.0.vendor:64 \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.0-service.oneplus_kona \
     libsensorndkbridge
 
 # Soong namespaces

AlphaDroid-devices/rmx3371_realme_sm8250-common

Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
Signed-off-by: DawfukFR <dawfukfr@gmail.com>

common.mk
@@ -418,7 +418,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.0-service.multihal_oplus \
     libsensorndkbridge \
     sensors.oplus
 