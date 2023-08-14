d8480d167184a3e50814872372fbea856a9387f4
bibindasb <bibindasappu1@gmail.com>
2023-08-14 13:07:37 +0530

    sm8250-common:Build android.hardware.thermal@2.0 04-09 01:52:25.581  1047  1047 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.qti.hardware.limits@1.0-service": library "android.hardware.thermal@2.0.so" not found: needed by main executable

--- common.mk
+++ common.mk
@@ -411,6 +411,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
 PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0 \
     android.hardware.thermal@2.0.vendor
 
 # QMI
