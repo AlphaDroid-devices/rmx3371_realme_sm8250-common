4006bf6d59d82a8d29ae6a2889b92550fe0d8e73
Amrutesh Naregal <107930548+DragonEmperor9480@users.noreply.github.com>
2023-08-28 19:25:17 +0530

    sm8250-common: adapt to lineage

--- common.mk
+++ common.mk
@@ -382,7 +382,7 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-bliss
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
